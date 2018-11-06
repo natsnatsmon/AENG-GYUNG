3d2154dda7bd14e497f87352cb4f78e772f3dfa3
JongWonCha <gagongin@kpu.ac.kr>
2018-11-07 00:01:33 +0900

    클라이언트 패킷통신 추가

--- ITS_MINE_Client/ITS_MINE_Client/ScnMgr.h
+++ ITS_MINE_Client/ITS_MINE_Client/ScnMgr.h
@@ -9,9 +9,6 @@ public:
 	~ScnMgr();
 
 	void RenderScene();
-	void Update(float eTime);
-	void ApplyForce(float x, float y, float eTime);
-	void Shoot(int shootID);
 	void AddObject(float x, float y, float z,
 		float sx, float sy,
 		float vx, float vy);
@@ -24,18 +21,6 @@ private:
 
 	GLuint m_TestTexture = 0;
 	GLuint m_TexSeq = 0;
-
-	GLuint m_testFloorTex = 0;
-	GLuint m_lifeTex = 0;
-	GLuint m_bulletTex = 0;
-	GLuint m_itemTex = 0;
-
-
-	// 플레이어는 지금 사용 안하고있습니당
-	GLuint m_player1Tex = 0;
-	GLuint m_player2Tex = 0;
-	GLuint m_player3Tex = 0;
-	GLuint m_player4Tex = 0;
 
 };
 
--- ITS_MINE_Client/ITS_MINE_Client/global.h
+++ ITS_MINE_Client/ITS_MINE_Client/global.h
@@ -1,17 +1,87 @@
 #pragma once
-
-#define SERVERIP   "59.16.87.178"		// 김정현 서버 시 사용
+#include <Windows.h>
+//#define SERVERIP   "127.0.0.1"
+#define SERVERIP   "59.16.87.178"
 #define SERVERPORT 8888
 
-#define HERO_ID 0 
+#define HERO_ID	0 
 
-#define MAX_OBJECTS 300
+#define MAX_OBJECTS	300
 
-#define SHOOT_NONE -1
-#define SHOOT_LEFT 1
-#define SHOOT_RIGHT 2
-#define SHOOT_UP 3
-#define SHOOT_DOWN 4
+#define SHOOT_NONE	-1
+#define SHOOT_LEFT	1
+#define SHOOT_RIGHT	2
+#define SHOOT_UP	3
+#define SHOOT_DOWN	4
 
-#define KIND_HERO 0
-#define KIND_BULLET 1
+#define KIND_HERO	0
+#define KIND_BULLET	1
+
+#define MAX_PLAYERS 2
+#define MAX_ITEMS 100
+#define MAX_POS 720
+
+#define R_PLAYER 40 // 플레이어 반지름
+#define R_ITEM 20 // 아이템 반지름
+
+#define INIT_POS -100.f
+#define INIT_LIFE 5
+
+#define SIZE_CToSPACKET 14
+#define SIZE_StoCPACKET 824
+
+//★ 게임 오버 스테이트를 두개로 나눌지 논의 필요
+enum gameState {
+	MainState, LobbyState, GamePlayState, GameOverState
+};
+
+//★ 플레이어 수는 임시로 4까지 해놓음. 끝날때까지도 2명만 쓰면 define으로 바꿈
+enum player {
+	player1, player2, player3, player4, nullPlayer = 99
+};
+
+#pragma pack(1)
+typedef struct Vec 
+{ float x; float y; };
+
+
+// Client -> Server
+typedef struct CtoSPacket
+{   
+	Vec pos;     
+	bool keyDown[4] = { 0, 0, 0, 0 };//스레드 동기화 왼쪽부터 시계방향
+	short life = 5;  
+}; 
+
+// Server -> Client 
+typedef struct StoCPacket 
+{  
+	DWORD time;
+	Vec p1Pos = { 0, 0 };
+	Vec p2Pos = { 0, 0 };
+
+	Vec itemPos[100];  
+	
+
+	short life;  
+	short gameState;
+};
+
+typedef struct ItemObj {
+	Vec pos;   // 아이템 위치 
+	bool isVisible;  // 화면 표시 여부
+
+	short playerID;
+};
+
+typedef struct Info {
+	short gameState;  // 게임 상태를 나타내는 변수 
+	DWORD gameTime;  // 게임 시간 
+
+	Vec player[2];
+	short life;
+	ItemObj item[100];  // 아이템 구조체
+}; 
+
+#pragma pack(4)
+
